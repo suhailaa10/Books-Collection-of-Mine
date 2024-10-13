138cebf01c61af2538f0062bbd607864b5e1e0bc
Suhaila Fathima <suhailasuhail98@gmail.com>
2024-10-13 11:32:31 +0530

    Update Connectdb.js

--- server/Connectdb.js
+++ server/Connectdb.js
@@ -2,7 +2,7 @@ const mongoose = require ("mongoose");
 const Connectdb = async () => {
     try{
         mongoose.set('strictQuery', false); // This will remove some unwanted warnings.
-        const conn = await mongoose.connect(process.env.Mongodb_Url);
+        const conn = await mongoose.connect("mongodb+srv://suhailaafathima:R3B0Tv2PiZkmGRYF@cluster0.bluwmdb.mongodb.net/Books?retryWrites=true&w=majority&appName=Cluster0");
         console.log(`Database Connected ${conn.connection.host}`);
     } catch(error){
         console.log(error);
